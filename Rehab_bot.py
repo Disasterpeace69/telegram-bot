3490cf3bd0ba6eda483a9b5172c427d35585c870
Disasterpeace69 <zyaadessam1@gmail.com>
2025-02-16 20:13:34 +0200

    Update Rehab_bot.py

--- Rehab_bot.py
+++ Rehab_bot.py
@@ -1,5 +1,6 @@
 from telegram import Update
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+from telegram.ext import Updater, CommandHandler, MessageHandler, CallbackContext
+import telegram.ext.filters as filters
 
 # دالة بدء البوت
 def start(update: Update, context: CallbackContext) -> None:
