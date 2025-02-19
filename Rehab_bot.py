bb4b0a154327f63579ea52f34c0d6055933c2cec
Disasterpeace69 <zyaadessam1@gmail.com>
2025-02-19 17:30:50 +0200

    Update Rehab_bot.py

--- Rehab_bot.py
+++ Rehab_bot.py
@@ -22,11 +22,11 @@ async def start(update: Update, context: CallbackContext) -> None:
         InputMediaPhoto("https://i.postimg.cc/9Qdvzzrq/Whats-App-Image-2025-02-18-at-5-17-00-PM.jpg"),  # استبدل بالرابط المباشر للصورة 4
         InputMediaPhoto("https://i.postimg.cc/BvQ402Mv/Whats-App-Image-2025-02-18-at-5-17-09-PM.jpg"),  # استبدل بالرابط المباشر للصورة 5
         InputMediaPhoto("https://i.postimg.cc/qRWTyS0t/Whats-App-Image-2025-02-16-at-11-41-28-PM.jpg"),  # استبدل بالرابط المباشر للصورة 6
-        InputMediaVideo("https://ia904509.us.archive.org/27/items/vid-20250218-170807-820/VID_20250218_170807_820.ia.mp4") #,  # استبدل بالرابط المباشر للفيديو 1
-        # InputMediaVideo("https://drive.google.com/uc?export=download&id=1berS2dwYYPqyZ7Mi3E458DSTlaP1-PJ1"),  # استبدل بالرابط المباشر للفيديو 2
-        # InputMediaVideo("https://drive.google.com/uc?export=download&id=1bdth4G-l-J3Nty32icZKTi20ZAgo7nos"),  # استبدل بالرابط المباشر للفيديو 3
-        # InputMediaVideo("https://drive.google.com/uc?export=download&id=1bda-VqD07rpV4TvIUrSZihm_wg459-bO")   # استبدل بالرابط المباشر للفيديو 4
-    ]
+    #     InputMediaVideo("https://ia904509.us.archive.org/27/items/vid-20250218-170807-820/VID_20250218_170807_820.ia.mp4") #,  # استبدل بالرابط المباشر للفيديو 1
+    #     # InputMediaVideo("https://drive.google.com/uc?export=download&id=1berS2dwYYPqyZ7Mi3E458DSTlaP1-PJ1"),  # استبدل بالرابط المباشر للفيديو 2
+    #     # InputMediaVideo("https://drive.google.com/uc?export=download&id=1bdth4G-l-J3Nty32icZKTi20ZAgo7nos"),  # استبدل بالرابط المباشر للفيديو 3
+    #     # InputMediaVideo("https://drive.google.com/uc?export=download&id=1bda-VqD07rpV4TvIUrSZihm_wg459-bO")   # استبدل بالرابط المباشر للفيديو 4
+    # ]
     await update.message.reply_media_group(media)
 
 # دالة إرسال تفاصيل الخدمات والأسعار
